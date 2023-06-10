5cb51a03b60881a155b5af2fcdf9ae4298bb010f
Jaqueline <jaquelinenacarate@gmail.com>
2023-06-10 17:59:27 +0000

    Parameterize queries for students.js and teachers.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -17,21 +17,35 @@ const pool = new Pool({
 // Queries
 //////////////////////////////////////////////////////////////////////////////
 
+// Values that come from somewhere else
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+// Query
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+
+// Send both parts to the database 
+
 // pool.query is a function that accepts an SQL query as a JavaScript string
 // The function then returns a promise that contains our result when the
 // query is successful
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then((res) => {
-  res.rows.forEach((user) => {
-    console.log(
-      `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
-    );
-  });
-})
-.catch((err) => console.error("query error", err.stack));
+pool
+  .query(queryString, values)
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+      );
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -17,23 +17,35 @@ const pool = new Pool({
 // Queries
 //////////////////////////////////////////////////////////////////////////////
 
-// pool.query is a function that accepts an SQL query as a JavaScript string
-// The function then returns a promise that contains our result when the
-// query is successful
-pool.query(`
+// Values that come from somewhere else
+const cohortName = process.argv[2] || "JUL02";
+
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+// Query
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher,
 cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ar ON ar.teacher_id = teachers.id
 JOIN students ON students.id = ar.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2] || "JUL02"}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
-.then((res) => {
-  res.rows.forEach(row => {
-    console.log(`
+`;
+
+// Send the query to the database 
+
+// pool.query is a function that accepts an SQL query as a JavaScript string
+// The function then returns a promise that contains our result when the
+// query is successful
+pool
+  .query(queryString, values)
+  .then((res) => {
+    res.rows.forEach((row) => {
+      console.log(`
   ${row.cohort}: ${row.teacher}`);
-  });
-})
-.catch((err) => console.error("query error", err.stack));
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
